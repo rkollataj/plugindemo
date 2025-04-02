9eda953419643302111e7b45508c079c77ab847e
Remigiusz Kollataj <remigiusz.kollataj@gmail.com>
2025-04-02 17:27:34 +0200

    change package name
    Signed-off-by: Remigiusz Kollataj <remigiusz.kollataj@gmail.com>

--- demo_test.go
+++ demo_test.go
@@ -6,7 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/traefik/plugindemo"
+	"github.com/rkollataj/plugindemo"
 )
 
 func TestDemo(t *testing.T) {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/traefik/plugindemo
+module github.com/rkollataj/plugindemo
 
 go 1.19
